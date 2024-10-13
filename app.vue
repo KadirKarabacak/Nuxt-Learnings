da11d1bd126b6c5ad9526f0c5a53018a3cb54fd0
Kadir <kadirht@hotmail.com>
2024-10-13 15:03:36 +0300

    chore:

--- app.vue
+++ app.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="h-screen w-screen bg-stone-800 text-white">
-   <NuxtLayout>
-    <ul>
-      <li>
-        <NuxtLink to="/">Home</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/events">Events</NuxtLink>
-      </li>
-    </ul>
-    <NuxtPage /> 
-   </NuxtLayout>
+  <div>
+    <NuxtLayout>
+      <!-- <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/events">Events</NuxtLink>
+        </li>
+      </ul> -->
+      <!-- <Profile />
+      <ProfileHeader />
+      <ProfileHeaderAvatar /> -->
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
