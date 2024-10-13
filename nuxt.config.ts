3de49e46cf9f0c71b589dc406e4a60ff28f966b9
Kadir <kadirht@hotmail.com>
2024-10-13 15:03:41 +0300

    chore:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
-import {resolve} from 'path'
+import { defineNuxtConfig } from 'nuxt/config';
+import { resolve } from 'path';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   alias: {
-    '@': resolve(__dirname, '/')
+    '@': resolve(__dirname, '/'),
   },
   css: ['~/assets/main.scss'],
   postcss: {
@@ -13,4 +14,4 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-})
+});
