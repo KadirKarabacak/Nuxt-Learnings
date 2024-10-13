1bf3d23a9a4fe3b1372de3decaf2bd83f006e08d
Kadir <kadirht@hotmail.com>
2024-10-13 16:59:38 +0300

    feat: create counter comp and use pinia store

---
+++ components/Counter.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const counter = useCounter()
+</script>
+
+<template>
+  <div>
+    Counter: {{ counter }}
+    <button @click="counter++">
+      +
+    </button>
+    <button @click="counter--">
+      -
+    </button>
+  </div>
+</template>
