ed1af0c2325c7927970d5cb4433330334ca6d775
Kadir <kadirht@hotmail.com>
2024-10-13 15:04:35 +0300

    feat: create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="DefaultLayout text-white bg-slate-900 h-screen">
+    Default Layout
+    <slot />
+  </div>
+  <!-- Near layout we have to display pages with <slot /> -->
+</template>
+
+<style lang="scss" scoped></style>
